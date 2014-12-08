6f0b6e608d3ed6118efe76b165f5f6d4df6a3300
Daniele Stroppa <stroppad@amazon.com>
2014-12-08 14:38:36 +0000

    Removed Nginx dependency

--- Berksfile
+++ Berksfile
@@ -2,5 +2,4 @@ source "https://supermarket.getchef.com"
 
 metadata
 
-cookbook 'nginx', '= 2.7.4'
 cookbook 'python', '= 1.4.6'
--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,3 @@ description       'Installs and configures Nginx and TornadoWeb for WebSockets'
 maintainer  	  'AWS OpsWorks'
 license           'Apache 2.0'
 version           '0.1.0'
-
-depends "ohai"
