83220e1c60ce231173e62119ff49fd4d13522462
Daniele Stroppa <stroppad@amazon.com>
2014-12-15 13:57:06 +0000

    Added custom python dpeloy

--- recipes/tornado_deploy.rb
+++ recipes/tornado_deploy.rb
@@ -14,6 +14,6 @@ node[:deploy].each do |application, deploy|
 
   python "chatdemo.py" do
 	cwd "#{deploy[:deploy_to]}/current"
-	only_if { ::File.exists?("#{deploy[:deploy_to]}/current/chatdemo.py") }
+	#only_if { ::File.exists?("#{deploy[:deploy_to]}/current/chatdemo.py") }
   end
 end
