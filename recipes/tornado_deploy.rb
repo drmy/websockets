4b153d0d22702a1fd192737f423447f6361dfaf4
Daniele Stroppa <stroppad@amazon.com>
2014-12-15 15:08:43 +0000

    Added custom python deploy

--- recipes/tornado_deploy.rb
+++ recipes/tornado_deploy.rb
@@ -13,7 +13,7 @@ node[:deploy].each do |application, deploy|
   end
 
   execute "run_demo" do
-	command "#{deploy[:deploy_to]}/current/chatdemo.py"
+	command "#{deploy[:deploy_to]}/current/chatdemo.py > #{deploy[:deploy_to]}/current/log/chatdemo.log &"
 	only_if { ::File.exists?("#{deploy[:deploy_to]}/current/chatdemo.py") }
   end
 end
